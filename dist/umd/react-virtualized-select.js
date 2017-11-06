d5f3963d5d31a3776645c9237e5fa80b247dab36
rjimenez <rjimenez@emagina.es>
2017-11-06 09:34:42 +0100

    Updated with new react-select changes.

--- dist/umd/react-virtualized-select.js
+++ dist/umd/react-virtualized-select.js
@@ -2162,6 +2162,7 @@
             }, {
                 key: "componentDidMount",
                 value: function() {
+                    //this.setPlacement();
                     "undefined" != typeof this.props.autofocus && "undefined" != typeof console && console.warn("Warning: The autofocus prop will be deprecated in react-select1.0.0 in favor of autoFocus to match React's autoFocus prop"), 
                     (this.props.autoFocus || this.props.autofocus) && this.focus();
                 }
@@ -2180,7 +2181,7 @@
                 key: "componentDidUpdate",
                 value: function(prevProps, prevState) {
                     // focus to the selected option
-                    if (this.setPlacement(), this.menu && this.focused && this.state.isOpen && !this.hasScrolledToOption) {
+                    if (this.menu && this.focused && this.state.isOpen && !this.hasScrolledToOption) {
                         var focusedOptionNode = _reactDom2.default.findDOMNode(this.focused), menuNode = _reactDom2.default.findDOMNode(this.menu);
                         menuNode.scrollTop = focusedOptionNode.offsetTop, this.hasScrolledToOption = !0;
                     } else this.state.isOpen || (this.hasScrolledToOption = !1);
@@ -2272,18 +2273,29 @@
                 value: function(event) {
                     // if the event was triggered by a mousedown and not the primary
                     // button, or if the component is disabled, ignore it.
-                    this.props.disabled || "mousedown" === event.type && 0 !== event.button || "INPUT" !== event.target.tagName && (// prevent default event handlers
-                    event.stopPropagation(), event.preventDefault(), // for the non-searchable select, toggle the menu
-                    /*if (!this.props.searchable) {
-	   	// TODO: This code means that if a select is searchable, onClick the options menu will not appear, only on subsequent click will it open.
-	   	this.focus();
-	   	return this.setState({
-	   		isOpen: true,
-	   	});
-	   }*/
-                    this.focus(), this.setState({
-                        isOpen: !0
-                    }));
+                    if (!(this.props.disabled || "mousedown" === event.type && 0 !== event.button) && "INPUT" !== event.target.tagName) {
+                        // for the non-searchable select, toggle the menu
+                        if (// prevent default event handlers
+                        event.stopPropagation(), event.preventDefault(), !this.props.searchable) // TODO: This code means that if a select is searchable, onClick the options menu will not appear, only on subsequent click will it open.
+                        return this.focus(), this.setState({
+                            isOpen: !0
+                        });
+                        if (this.state.isFocused) {
+                            // On iOS, we can get into a state where we think the input is focused but it isn't really,
+                            // since iOS ignores programmatic calls to input.focus() that weren't triggered by a click event.
+                            // Call focus() again here to be safe.
+                            this.focus();
+                            var input = this.input;
+                            "function" == typeof input.getInput && (// Get the actual DOM input if the ref is an <AutosizeInput /> component
+                            input = input.getInput()), // clears the value so that the cursor will be at the end of input when the component re-renders
+                            input.value = "", // if the input is focused, ensure the menu is open
+                            this.setState({
+                                isOpen: !0,
+                                isPseudoFocused: !1
+                            });
+                        } else // otherwise, focus the input and open the menu
+                        this._openAfterFocus = this.props.openOnClick, this.focus();
+                    }
                 }
             }, {
                 key: "handleMouseDownOnArrow",
@@ -2961,13 +2973,13 @@
                 value: function(scrolling) {
                     var targetEl = null;
                     this._visibleOptions = this.filterOptions(this.props.multi ? this.getValueArray(this.props.value) : null);
-                    if ((this.state.isOpen || this.props.isOpen) && (targetEl = this.refs.layer ? this.refs.layer.getLayer().children[0] : null)) {
+                    if ((this.state.isOpen || this.props.isOpen) && (targetEl = this.refs.layer && this.refs.layer.getLayer() ? this.refs.layer.getLayer().children[0] : null)) {
                         var anchor = this.getAnchorPosition(_reactDom2.default.findDOMNode(this.wrapper));
                         this.getTargetPosition(targetEl);
                         scrolling && this.props.autoCloseWhenOffScreen && this.autoCloseWhenOffScreen(anchor), 
                         targetEl.style.top = Math.max(0, anchor.bottom) + "px", targetEl.style.left = Math.max(0, anchor.left) + "px", 
                         targetEl.style.maxHeight = window.innerHeight + "px", targetEl.style.position = "fixed", 
-                        targetEl.style.width = Math.max(0, anchor.width) + "px", console.log("Set Placement", targetEl);
+                        targetEl.style.width = Math.max(0, anchor.width) + "px";
                     }
                 }
             }, {
@@ -3000,7 +3012,7 @@
                         className: "Select-aria-only",
                         "aria-live": "assertive"
                     }, this.props.backspaceToRemoveMessage.replace("{label}", valueArray[valueArray.length - 1][this.props.labelKey]))), 
-                    console.log("Render de Select.js...", isOpen), _react2.default.createElement("div", {
+                    _react2.default.createElement("div", {
                         ref: function(_ref7) {
                             return _this10.wrapper = _ref7;
                         },
@@ -3020,7 +3032,7 @@
                     }, _react2.default.createElement("span", {
                         className: "Select-multi-value-wrapper",
                         id: this._instancePrefix + "-value"
-                    }, this.renderValue(valueArray, isOpen), this.renderInput(valueArray, focusedOptionIndex)), removeMessage, this.renderLoading(), this.renderClear(), this.renderArrow()), isOpen ? _react2.default.createElement("div", null, _react2.default.createElement(_reactEventListener2.default, {
+                    }, this.renderValue(valueArray, isOpen), this.renderInput(valueArray, focusedOptionIndex)), removeMessage, this.renderLoading(), this.renderClear(), this.renderArrow()), _react2.default.createElement("div", null, _react2.default.createElement(_reactEventListener2.default, {
                         target: "window",
                         onScroll: this.handleScroll,
                         onResize: this.handleResize
@@ -3029,8 +3041,9 @@
                         open: isOpen,
                         componentClickAway: this.componentClickAway,
                         useLayerForClickAway: !1,
+                        parentComponent: this.wrapper,
                         render: this.renderOuter(options, valueArray, focusedOption)
-                    })) : null);
+                    })));
                 }
             } ]), Select;
         }(_react2.default.Component);
@@ -4493,19 +4506,24 @@
                 return protoProps && defineProperties(Constructor.prototype, protoProps), staticProps && defineProperties(Constructor, staticProps), 
                 Constructor;
             };
-        }(), _react = __webpack_require__(106), _reactDom = __webpack_require__(109), _dom = __webpack_require__(130), _dom2 = _interopRequireDefault(_dom), RenderToLayer = function(_Component) {
+        }(), _reactDom = __webpack_require__(109), _reactDom2 = _interopRequireDefault(_reactDom), _react = __webpack_require__(106), _dom = __webpack_require__(130), _dom2 = _interopRequireDefault(_dom), RenderToLayer = function(_Component) {
             function RenderToLayer() {
                 return _classCallCheck(this, RenderToLayer), _possibleConstructorReturn(this, (RenderToLayer.__proto__ || Object.getPrototypeOf(RenderToLayer)).apply(this, arguments));
             }
             return _inherits(RenderToLayer, _Component), _createClass(RenderToLayer, [ {
-                key: "componentDidMount",
-                value: function() {
-                    this.renderLayer();
+                key: "componentWillUpdate",
+                value: function(nextProps, nextState) {
+                    this.renderLayer(nextProps.open, nextProps.render, nextProps.parentComponent);
                 }
             }, {
                 key: "componentDidUpdate",
                 value: function() {
-                    this.renderLayer();
+                    this.renderLayer(this.props.open, this.props.render, this.props.parentComponent);
+                }
+            }, {
+                key: "componentWillReceiveProps",
+                value: function(nextProps, nextState) {
+                    nextProps.open !== this.props.open && this.renderLayer(nextProps.open, nextProps.render, nextProps.parentComponent);
                 }
             }, {
                 key: "componentWillUnmount",
@@ -4535,19 +4553,19 @@
                 }
             }, {
                 key: "renderLayer",
-                value: function() {
-                    var _this2 = this, _props = this.props, open = _props.open, render = _props.render;
+                value: function(open, render, parentComponent) {
                     if (open) {
-                        this.layer || (console.log("renderLayer, open true, !this.layer"), this.layer = document.createElement("div"), 
-                        document.body.appendChild(this.layer), this.props.useLayerForClickAway ? (this.layer.addEventListener("touchstart", this.onClickAway), 
-                        this.layer.addEventListener("click", this.onClickAway), this.layer.style.position = "fixed", 
-                        this.layer.style.top = 0, this.layer.style.bottom = 0, this.layer.style.left = 0, 
-                        this.layer.style.right = 0, this.layer.style.zIndex = 1e4) : setTimeout(function() {
-                            window.addEventListener("touchstart", _this2.onClickAway), window.addEventListener("click", _this2.onClickAway);
-                        }, 0));
+                        if (!this.layer) {
+                            this.layer = document.createElement("div"), document.body.appendChild(this.layer);
+                            var rect = _reactDom2.default.findDOMNode(parentComponent).getBoundingClientRect();
+                            this.layer.addEventListener("touchstart", this.onClickAway), this.layer.addEventListener("click", this.onClickAway), 
+                            this.layer.style.position = "fixed", this.layer.style.top = Math.max(0, rect.bottom) + "px", 
+                            this.layer.style.left = Math.max(0, rect.left) + "px", this.layer.style.width = Math.max(0, rect.width) + "px", 
+                            this.layer.style.zIndex = 1e4;
+                        }
                         var layerElement = render;
                         this.layerElement = (0, _reactDom.unstable_renderSubtreeIntoContainer)(this, layerElement, this.layer);
-                    } else console.log("renderLayer, not open"), this.unrenderLayer();
+                    } else this.unrenderLayer();
                 }
             }, {
                 key: "render",
